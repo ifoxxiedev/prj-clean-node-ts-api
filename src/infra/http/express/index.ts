3c75510ff84626a49a852a10962b839488f7b395
ifoxxiedev <obede.drrt.info@gmail.com>
2022-10-03 16:30:29 -0300

    refactor: final todo project

--- src/infra/http/express/index.ts
+++ src/infra/http/express/index.ts
@@ -1,6 +1,6 @@
 import { CreateTodoUseCase } from '@src/application/create-todo.use-case';
 import { ListTodoUseCase } from '@src/application/list-todo-use-case';
-import { TodoInMemoryRepository } from '@src/infra/db/todo-in-memory-repository';
+import { TodoPrismaRepository } from '@src/infra/db/prisma/todo-repository';
 import express from 'express';
 
 const app = express();
@@ -8,20 +8,22 @@ app.use(express.json())
 const port = process.env.PORT || 3333;
 
 
-const todoRepository = new TodoInMemoryRepository();
+export const setupExpress = () => {
+  const todoRepository = new TodoPrismaRepository();
 
-app.post('/todo', async (req, res, next) => {
-  const useCase = new CreateTodoUseCase(todoRepository);
-  const output = await useCase.execute(req.body);
-  res.status(201).json(output);
-});
+  app.post('/todo', async (req, res, next) => {
+    const useCase = new CreateTodoUseCase(todoRepository);
+    const output = await useCase.execute(req.body);
+    res.status(201).json(output);
+  });
 
-app.get('/todo', async (req, res, next) => {
-  // Let's go implement.
-  const useCase = new ListTodoUseCase(todoRepository);
-  const output = await useCase.execute();
-  res.status(200).json(output);
-});
+  app.get('/todo', async (req, res, next) => {
+    // Let's go implement.
+    const useCase = new ListTodoUseCase(todoRepository);
+    const output = await useCase.execute();
+    res.status(200).json(output);
+  });
 
-app.listen(port)
-console.log(`Express is running on ${port}`)
+  app.listen(port)
+  console.log(`Express is running on ${port}`)
+}
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,9 @@
 import { PrismaConnector } from "@src/infra/db/prisma/connector"
+import { setupExpress } from "@src/infra/http/express";
 
 const main = async () => {
   await PrismaConnector.connect()
+  setupExpress()
 }
 
 main();
